b9de4baca442b9a12f0c5132f982d40221310c4e
uttom <uttombarmondev@gmail.com>
2025-04-25 23:24:28 +0600

    frontend common layout added

---
+++ frontend/app/(CommonLayout)/layout.tsx
@@ -0,0 +1,46 @@
+// import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "VirtualStore",
+//   icons: {
+//     icon: "/favicon.ico",
+//     shortcut: "/favicon.ico",
+//     apple: "/favicon.ico",
+//   },
+//   openGraph: {
+//     title: "VirtualStore",
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "VirtualStore",
+//     description:
+//       " Discover a wide range of products online at Virtual Store, your trusted e-commerce destination in Bangladesh. Enjoy convenient shopping and secure delivery right to your doorstep in Dinajpur and across the nation.",
+//     images: "/images/og-image.png",
+//   },
+//   keywords: [
+//     "Virtual store",
+//     "virtual",
+//     "store",
+//     "virtualstore",
+//     "e-commerce",
+//     "price",
+//   ],
+//   description:
+//     "Discover a wide range of products online at Virtual Store, your trusted e-commerce destination in Bangladesh. Enjoy convenient shopping and secure delivery right to your doorstep in Dinajpur and across the nation.",
+
+import Navbar from "@/components/shared/Navbar";
+
+// };
+export default function CommonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      {children}
+    </div>
+  );
+}
---
+++ frontend/components/shared/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <p className=" font-[family-name:var(--font-open-sans)] font-bold text-lg">
+          Virtual Store
+        </p>
+      </div>
+    </div>
+  );
+}
